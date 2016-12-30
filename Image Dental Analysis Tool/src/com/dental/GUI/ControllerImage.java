c9e27e651caddb75bdc59ef47db1c8c39fa1f187
MizanQistina <inamizan@gmail.com>
2016-12-30 19:45:40 +0800

    Neccessary packages are imported to ControllerImage.java. Edited README - the version of OpenCV used is stated.

--- Image Dental Analysis Tool/src/com/dental/GUI/ControllerImage.java
+++ Image Dental Analysis Tool/src/com/dental/GUI/ControllerImage.java
@@ -1,17 +1,23 @@
 package com.dental.GUI;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class ControllerImage extends Main implements Initializable {
@@ -31,29 +37,36 @@ public class ControllerImage extends Main implements Initializable {
 	}	
 		
 	@FXML
-	void onClickBrowse() throws IOException
-	{		
-	FileChooser chooser = new FileChooser();
+	void onClickBrowse() throws IOException {		
+		
+		FileChooser chooser = new FileChooser();
     	chooser.setTitle("Choose image");
-    	chooser.getExtensionFilters().add(
-    	new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")//File format restrictions
-    	);             		
+    	
+    	// File format restrictions - .png, .jpg, .jpeg
+    	chooser.getExtensionFilters().add( new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+    	
     	File Selectedfile = chooser.showOpenDialog(new Stage());
-    	BufferedImage bimg = ImageIO.read(Selectedfile);//Taking file and storing it as image
-    	int width          = bimg.getWidth();			//getting dimensions of the image	
+    	
+    	// Taking file and storing it as image
+    	BufferedImage bimg = ImageIO.read(Selectedfile);
+    	
+    	// Getting dimensions of the image
+    	int width          = bimg.getWidth();				
     	int height         = bimg.getHeight();
-    	System.out.println("The width of the image is:"+width+"The height of the image is:"+height);//For developer reference
-    	if(width>1366&&height>768||width>1366||height>768)//Check if the image size is acceptable
-    		{
-    		Alert alert = new Alert(AlertType.ERROR);//Alert box appears when if statement is true    		
+    	
+    	// For developer reference
+    	System.out.println("The width of the image is:"+width+"The height of the image is:"+height);
+    	
+    	// Check if the image size is acceptable
+    	if(width>1366&&height>768||width>1366||height>768) {
+    		
+    		// Alert box appears when if statement is true
+    		Alert alert = new Alert(AlertType.ERROR);    		
     		alert.setTitle("Error");
     		alert.setHeaderText("Uploaded Failed");
     		alert.setContentText("Upload image less than 1.1 mbs");
     		alert.showAndWait();
-    		}
-    	
-    
-    
+    	}
 	}
 
 	@FXML
