4ac34f09c9f47dde1a91617aed64b5144505222e
Unknown <abirami96@gmail.com>
2017-02-24 00:25:36 +0800

    Removed Edit from Menu Bar

--- Image Dental Analysis Tool/src/application/Main.java
+++ Image Dental Analysis Tool/src/application/Main.java
@@ -1,21 +1,20 @@
-package com.dental.GUI;
+package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 
 public class Main extends Application {
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("GUI_Image.fxml"));			
-			Scene scene = new Scene(root);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();	
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
